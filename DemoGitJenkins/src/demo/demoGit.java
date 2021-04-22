677c02ad6d71345c8ef2a82796e494afa78a096e
artidd0212 <82807232+artidd0212@users.noreply.github.com>
2021-04-22 11:09:25 +0530

    changes by arti

--- DemoGitJenkins/src/demo/demoGit.java
+++ DemoGitJenkins/src/demo/demoGit.java
@@ -2,6 +2,10 @@ package demo;
 
 public class demoGit 
 {
+	public void add()
+	{ 
+	      System.out.println("hello add method");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("hello git_jenkins");
